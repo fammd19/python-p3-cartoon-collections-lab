74cc7f1b6b7d12d99b1d5f9dc2b71d91e02e65df
Fiona Mellink <fimellink@gmail.com>
2024-05-06 15:37:15 +1000

    Complete lab

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,46 @@
-def roll_call_dwarves():
-    pass
+def roll_call_dwarves(names):
+    i=1
+    for name in names:
+        print(f"{i}. {name}")
+        i+=1
 
-def summon_captain_planet():
-    pass
+dwarves = ["Doc", "Dopey", "Bashful", "Grumpy"]
+roll_call_dwarves(dwarves)
 
-def long_planeteer_calls():
-    pass
+def summon_captain_planet(calls):
+    updated = [x[0].upper() + x[1:] + "!" for x in calls]
+    print(updated)
+    return updated
 
-def find_the_cheese():
-    pass
+planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
+summon_captain_planet(planeteer_calls)
+
+def long_planeteer_calls(calls):
+    for call in calls:
+        if len(call) >= 4:
+            print(True)
+            return False
+        else:
+            print(False)
+            return True
+
+short_words = ["puff", "go", "two"]
+long_planeteer_calls(short_words)
+
+assorted_words = ["two", "go", "industrious", "bop"]
+long_planeteer_calls(assorted_words)
+
+def find_the_cheese(foods):
+    for food in foods:
+        if food in ["cheddar", "gouda", "camembert"]:
+            print(food)
+            return food
+            break
+    else:
+        None
+        
+snacks = ["crackers", "gouda", "thyme"]
+find_the_cheese(snacks)
+
+soup = ["tomato soup", "cheddar", "oyster crackers", "gouda"]
+find_the_cheese(soup)
